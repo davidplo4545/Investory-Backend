bc2de14e9c098d92b91de65274b66dfa90b3e56c
david plotkin <davidplo4545@gmail.com>
2021-07-11 14:18:43 +0300

    Added django-venv settings

--- longterm/longterm/settings.py
+++ longterm/longterm/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.env()
+environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-son^2v+ue*-kq*pqkiz&((-4_1gw0xgw5cr*7g#5-$e&=2m#dt'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
