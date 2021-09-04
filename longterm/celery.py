0bbf6b1d1f68f8c88ecad303787608b26eace855
david plotkin <davidplo4545@gmail.com>
2021-09-04 23:40:02 +0300

    removed tasks

--- longterm/celery.py
+++ longterm/celery.py
@@ -17,21 +17,21 @@ app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
 #                 CELERY_RESULT_BACKEND=os.environ['CLOUDAMQP_URL'])
 
 app.conf.beat_schedule = {
-    'scrape us stocks': {
-        'task': 'api.tasks.scrape_us_stocks',
-        'schedule': crontab(day_of_week='mon,tue,wed,thu,fri,sat',
-                            hour='17-0',
-                            minute='*/15'
-                            ),
-        'options': {'queue': 'default'},
-    },
-    'scrape israeli stocks': {
-        'task': 'api.tasks.scrape_isr_stocks',
-        'schedule': crontab(day_of_week='sun,mon,tue,wed,thu,sat',
-                            hour='10-0',
-                            minute='*/8'
-                            ),
-        'options': {'queue': 'default'},
+    # 'scrape us stocks': {
+    #     'task': 'api.tasks.scrape_us_stocks',
+    #     'schedule': crontab(day_of_week='mon,tue,wed,thu,fri,sat',
+    #                         hour='17-0',
+    #                         minute='*/15'
+    #                         ),
+    #     'options': {'queue': 'default'},
+    # },
+    # 'scrape israeli stocks': {
+    #     'task': 'api.tasks.scrape_isr_stocks',
+    #     'schedule': crontab(day_of_week='sun,mon,tue,wed,thu,sat',
+    #                         hour='10-0',
+    #                         minute='*/8'
+    #                         ),
+    #     'options': {'queue': 'default'},
     },
     # updates portfolio every day, every hour between 10:00 - 00:00
     # 'update portfolios': {
